2b08aa9d71a8f2a5173fb2f10b7d7d9ba10c866e
naochanglove <108738818+naochanglove@users.noreply.github.com>
2022-07-05 22:04:45 +0700

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,33 @@
+let opened = false;
+let openedA = false;
+let openedB = false;
 
+function openData() {
+    if (!opened) {
+        document.getElementById("dua").style.display = 'block';
+        opened = true;
+    } else {
+        document.getElementById("dua").style.display = 'none';
+        opened = false;
+    }
+};
+
+function openDataA() {
+    if (!openedA) {
+        document.getElementById("muncul").style.display = 'block';
+        openedA = true;
+    } else {
+        document.getElementById("muncul").style.display = 'none';
+        openedA = false;
+    }
+};
+
+function openDataB() {
+    if (!openedB) {
+        document.getElementById("tiga").style.display = 'block';
+        openedB = true;
+    } else {
+        document.getElementById("tiga").style.display = 'none';
+        openedB = false;
+    }
+};
